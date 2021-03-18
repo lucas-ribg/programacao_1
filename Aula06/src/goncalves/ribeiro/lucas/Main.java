f85389240ede743da8ce6b383f06093146bbbfdf
Lucas Ribeiro <lucas123.r.g@gmail.com>
2021-03-18 09:14:57 -0300

    Aula 06

---
+++ Aula06/src/goncalves/ribeiro/lucas/Main.java
@@ -0,0 +1,11 @@
+package goncalves.ribeiro.lucas;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String cpf = "529.761.188-17";
+        System.out.println("CPF é valido: " + validadorCPF.validarCPF(cpf));
+
+    }
+}
