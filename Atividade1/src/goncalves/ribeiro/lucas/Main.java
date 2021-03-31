81da65a11be91414a230e7c30991b258e62a95da
Lucas Ribeiro <lucas123.r.g@gmail.com>
2021-03-30 23:36:03 -0300

    Main

---
+++ Atividade1/src/goncalves/ribeiro/lucas/Main.java
@@ -0,0 +1,37 @@
+package goncalves.ribeiro.lucas;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int op;
+        Scanner scanner;
+        String usuario;
+        int valor;
+        String QRcode;
+        Scanner scanner;
+
+        System.out.println("1. Pagar");
+        System.out.println("2. Receber");
+        scanner = new Scanner(System.in);
+
+        op = scanner.nextInt();
+
+        if (op == 1){
+            System.out.println("Informe o destinatário:");
+            usuario = scanner.nextLine();
+
+            System.out.println("Informe o valor:");
+            valor = scanner.nextInt();
+
+            System.out.println("Informe o QRCode: ");
+            QRcode = scanner.nextLine();
+
+        }
+        if (op == 2){
+            gerarChave();
+        }
+    }
+
+}
