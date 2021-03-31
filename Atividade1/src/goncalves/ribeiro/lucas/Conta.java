a5bc814cc725eb31594b5b74de5010c1210dc1d5
Lucas Ribeiro <lucas123.r.g@gmail.com>
2021-03-31 12:08:29 -0300

    classe usuario

--- Atividade1/src/goncalves/ribeiro/lucas/Conta.java
+++ Atividade1/src/goncalves/ribeiro/lucas/Conta.java
@@ -3,5 +3,6 @@ package goncalves.ribeiro.lucas;
 public class Conta {
     private String idConta;
     private int saldo;
+    private Usuario usuario;
 
 }
--- Atividade1/src/goncalves/ribeiro/lucas/Usuario.java
+++ Atividade1/src/goncalves/ribeiro/lucas/Usuario.java
@@ -6,4 +6,20 @@ public class Usuario {
     private String email;
     private String CPF;
 
+    //construtor
+    public Usuario(String nome, String senha, String CPF) {
+        this.nome = nome;
+        this.senha = senha;
+        this.CPF = CPF;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nome='" + nome + '\'' +
+                ", senha='" + senha + '\'' +
+                ", email='" + email + '\'' +
+                ", CPF='" + CPF + '\'' +
+                '}';
+    }
 }
