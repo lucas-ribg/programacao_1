29b3a6e8e68029211103c56d8eb183d103206ded
Lucas Ribeiro <lucas123.r.g@gmail.com>
2021-06-21 14:06:03 -0300

    Main

---
+++ T2/src/br/maua/Main.java
@@ -0,0 +1,12 @@
+package br.maua;
+
+import br.maua.models.MainSystem;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MainSystem MAsK_S0c13ty = new MainSystem();
+        MAsK_S0c13ty.execute();
+    }
+}
