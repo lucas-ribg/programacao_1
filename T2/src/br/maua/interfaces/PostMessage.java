25d05bc4d92298ac7070dad78e13d1b46204636e
Lucas Ribeiro <lucas123.r.g@gmail.com>
2021-06-27 18:04:04 -0300

    Update PostMessage.java

--- T2/src/br/maua/interfaces/PostMessage.java
+++ T2/src/br/maua/interfaces/PostMessage.java
@@ -2,6 +2,9 @@ package br.maua.interfaces;
 
 import br.maua.enums.OfficeHour;
 
+/**
+ * Interface com um método para o membro postar uma messagem
+ */
 public interface PostMessage {
     String postMessage(OfficeHour hour);
 }
