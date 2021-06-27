ca20385be30961b80b7dfee9a946a7b313f2905a
Lucas Ribeiro <lucas123.r.g@gmail.com>
2021-06-27 19:06:32 -0300

    postMessage

--- T2/src/br/maua/models/MainSystem.java
+++ T2/src/br/maua/models/MainSystem.java
@@ -117,15 +117,18 @@ public class MainSystem {
             boolean aux1 = true;
             System.out.println("Digite o ID:");
             String ID = scanner.nextLine();
+
             while (aux1) {
                 Scanner y = new Scanner(csvTest);
                 y.useDelimiter("[;\n]");
                 String Teste = "";
+
                 while (y.hasNext()) {
                     String idTest = y.next();
                     y.next();
                     y.next();
                     y.next();
+
                     if (idTest.equals(ID)) {
                         System.out.println("ID já existe!\nEscolha outro:");
                         Teste = "existe";
@@ -143,12 +146,15 @@ public class MainSystem {
             System.out.println("Digite o email:");
             String email = scanner.nextLine();
             String role;
+
             while(true) {
+
                 System.out.println("Escolha a função:\n");
                 System.out.println("0 - Mobile Member");
                 System.out.println("1 - Heavy Lifter");
                 System.out.println("2 - Script Guy");
                 System.out.println("3 - Big Brother");
+
                 int op = Integer.parseInt(scanner.nextLine());
                 if (op == 0) {
                     role = "MOBILE_MEMBERS";
@@ -187,14 +193,107 @@ public class MainSystem {
         }
     }
 
-    public void fireMember(){
+    /**
+     * Deleta uma linha no "arquivo_super_Secreto_nao_abrir.csv" de acordo com o ID escolhido pelo usuário.
+     * Faz isso criando o arquivo "temporario.csv" e copiando os novos dados nele. Assim que finalizado ele apaga o arquivo antigo e
+     * renomeia o arquivo temporario para o nome passado pelo parâmetro fileName.
+     * @param fileName nome do arquivo em que serão gravados os novos dados sem a linha com o ID selecionado.
+     */
+    public void fireMember(String fileName){
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o ID do membro que sera demitido:");
+        String numID = scanner.nextLine();
+        String tempFile = "temporario.csv";
+        String ID; String name; String email; String role;
+
+        try{
+            FileWriter fw = new FileWriter(tempFile, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            File csvTest = path;
+            Scanner y = new Scanner(csvTest);
+            y.useDelimiter("[;\n]");
+
+            while(y.hasNext()){
+                ID = y.next();
+                name = y.next();
+                email = y.next();
+                role = y.next();
+
+                if(!ID.equals(numID)){
+                    pw.print(ID + ";" + name + ";" + email + ";" + role + "\n");
+                }
+            }
+            y.close();
+            pw.flush();
+            pw.close();
+            bw.close();
+            fw.close();
+            new File(fileName).delete();
+            File novoNome = new File(fileName);
+            new File(tempFile).renameTo(novoNome);
+        }
+        catch (Exception e){
+            JOptionPane.showMessageDialog(null,"Erro");
+        }
     }
 
+    /**
+     * Exibe qual o tipo de horário em que o usuário está.
+     */
     public void hourType(){
+        System.out.println("Você está em horário "+ hour + "!\n");
 
     }
 
+    /**
+     * Posta uma mensagem para cada membro presente em "arquivo_super_Secreto_nao_abrir.csv" de acordo com a role.
+     * @param hour hora em que os membros estão trabalhando, diferenciando qual tipo de mensagem cada role irá retornar.
+     */
+    public void postMessage(OfficeHour hour) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String itemCsv = br.readLine();
+
+            while(itemCsv != null){
+
+                String[] itemSeparado = itemCsv.split(";");
+                String ID = itemSeparado[0];
+                String name = itemSeparado[1];
+                String email = itemSeparado[2];
+                String role = itemSeparado[3];
+
+                if (role.equals("MOBILE_MEMBERS")) {
+
+                    MobileMembers temp = new MobileMembers(Integer.parseInt(ID), name, email, Roles.MOBILE_MEMBERS);
+                    System.out.println(temp.presentation(temp) + ": " + temp.postMessage(hour));
+
+                } else if (role.equals("HEAVY_LIFTERS")) {
+
+                    HeavyLifters temp = new HeavyLifters(Integer.parseInt(ID), name, email, Roles.HEAVY_LIFTERS);
+                    System.out.println(temp.presentation(temp) + ": " + temp.postMessage(hour));
+
+                } else if (role.equals("SCRIPT_GUYS")) {
+
+                    ScriptGuys temp = new ScriptGuys(Integer.parseInt(ID), name, email, Roles.SCRIPT_GUYS);
+                    System.out.println(temp.presentation(temp) + ": " + temp.postMessage(hour));
+
+                } else if (role.equals("BIG_BROTHERS")) {
+
+                    BigBrothers temp = new BigBrothers(Integer.parseInt(ID), name, email, Roles.BIG_BROTHERS);
+                    System.out.println(temp.presentation(temp) + ": " + temp.postMessage(hour));
+
+                }
+                itemCsv = br.readLine();
+            }
+            br.close();
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null,"Erro");
+        }
+    }
+
+    
 }
 
 
