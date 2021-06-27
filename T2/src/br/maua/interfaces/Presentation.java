1ef944498918de6dcf924b4dc837cb9d6649de0f
Lucas Ribeiro <lucas123.r.g@gmail.com>
2021-06-27 18:04:58 -0300

    Update Presentation.java

--- T2/src/br/maua/interfaces/Presentation.java
+++ T2/src/br/maua/interfaces/Presentation.java
@@ -2,6 +2,9 @@ package br.maua.interfaces;
 
 import br.maua.models.Members;
 
+/**
+ * Interface que possui um método para um membro se apresentar
+ */
 public interface Presentation {
     String presentation(Members members);
 }
