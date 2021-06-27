8cc7be181f3419ee5497f94a967ecc9fb713f1a2
Lucas Ribeiro <lucas123.r.g@gmail.com>
2021-06-27 17:51:49 -0300

    Update HeavyLifters.java

--- T2/src/br/maua/models/HeavyLifters.java
+++ T2/src/br/maua/models/HeavyLifters.java
@@ -1,5 +1,6 @@
 package br.maua.models;
 
+import br.maua.enums.OfficeHour;
 import br.maua.enums.Roles;
 
 public class HeavyLifters extends Members{
@@ -8,4 +9,17 @@ public class HeavyLifters extends Members{
         super(ID, name, email, role);
     }
 
+    @Override
+    public String postMessage(OfficeHour hour) {
+        if(hour == OfficeHour.REGULAR){
+            return("Podem contar conosco!");
+        } else{
+            return("N00b_qu3_n_Se_r3pita.bat");
+        }
+    }
+
+    @Override
+    public String presentation(Members member) {
+        return("ID:" + member.getID() + " | " + member.getEmail() + " - " + member.getName()+ "(" + member.getRole() + ")");
+    }
 }
