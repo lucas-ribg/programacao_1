793b140da02d72c2e36da347a3ccd24d055b7272
Lucas Ribeiro <lucas123.r.g@gmail.com>
2021-06-27 18:14:38 -0300

    Update HeavyLifters.java

--- T2/src/br/maua/models/HeavyLifters.java
+++ T2/src/br/maua/models/HeavyLifters.java
@@ -3,12 +3,20 @@ package br.maua.models;
 import br.maua.enums.OfficeHour;
 import br.maua.enums.Roles;
 
+/**
+ * Classe que representa o cargo dos HeavyLifters e extende a classe membros
+ */
 public class HeavyLifters extends Members{
 
     public HeavyLifters(int ID, String name, String email, Roles role) {
         super(ID, name, email, role);
     }
 
+    /**
+     * Método que posta a mensagem pelo membro em seu respectivo horario de trabalho
+     * @param hour Horátio de trabalho em que se encontra
+     * @return retorna a mensagem a ser postada pelo membro
+     */
     @Override
     public String postMessage(OfficeHour hour) {
         if(hour == OfficeHour.REGULAR){
@@ -18,6 +26,11 @@ public class HeavyLifters extends Members{
         }
     }
 
+    /**
+     * Método que realiza a apresentação do membro
+     * @param member Membro referente a apresentação
+     * @return retorna a apresentação do membro
+     */
     @Override
     public String presentation(Members member) {
         return("ID:" + member.getID() + " | " + member.getEmail() + " - " + member.getName()+ "(" + member.getRole() + ")");
