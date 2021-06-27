525c0c0b09279d82b48cb9e9d35ef0c822440334
Lucas Ribeiro <lucas123.r.g@gmail.com>
2021-06-27 17:37:44 -0300

    update OfficeHour

--- T2/src/br/maua/enums/OfficeHour.java
+++ T2/src/br/maua/enums/OfficeHour.java
@@ -1,11 +1,5 @@
 package br.maua.enums;
 
 public enum OfficeHour {
-    REGULAR(0), EXTRA(1);
-
-    private int code;
-
-    OfficeHour(int code) {
-        this.code = code;
-    }
+    REGULAR, EXTRA;
 }
